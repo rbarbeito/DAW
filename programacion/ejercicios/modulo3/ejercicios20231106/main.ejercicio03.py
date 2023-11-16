bbdda4a99373fba3d62217e99e9c8608a32874f0
Rodolfo Barbeito <rbarbeito@gmail.com>
2023-11-16 13:11:14 +0100

    Correcion del ejercico de sumas de filas y columnas 5x5

--- programacion/ejercicios/modulo3/ejercicios20231106/main.ejercicio03.py
+++ programacion/ejercicios/modulo3/ejercicios20231106/main.ejercicio03.py
@@ -13,61 +13,35 @@ print("*"*long)
 print(" "*margen_izquierdo + titulo)
 print("*"*long)
 
-fila1 = []
-fila2 = []
-fila3 = []
-fila4 = []
-fila5 = []
+list_total=[]
+
 
 for i in range(5):
-    x = 0
-    while x < 5:
+    lista=[]
+    for x in range(5):
         enunciado = "Teclee el valor " + \
             str(x+1) + " de la fila " + str(i+1) + ": "
         nuevo_numero = int(input(enunciado))
 
-        if i == 0:
-            fila1.append(nuevo_numero)
-        elif i == 1:
-            fila2.append(nuevo_numero)
-        elif i == 2:
-            fila3.append(nuevo_numero)
-        elif i == 3:
-            fila4.append(nuevo_numero)
-        elif i == 4:
-            fila5.append(nuevo_numero)
+        lista.append(nuevo_numero)
 
-        x += 1
-
+    list_total.append(lista)
+    
 print()
-print(fila1)
-print(fila2)
-print(fila3)
-print(fila4)
-print(fila5)
+print(list_total)
 
 
-suma_fila1 = suma_fila2 = suma_fila3 = suma_fila4 = suma_fila5 = 0
-lista_suma_columna = []
+lista_suma_filas=[]
+lista_suma_columnas=[]
+
 for i in range(5):
-    suma_fila1 += fila1[i]
-    suma_fila2 += fila2[i]
-    suma_fila3 += fila3[i]
-    suma_fila4 += fila4[i]
-    suma_fila5 += fila5[i]
-
-    lista_suma_columna.append(fila1[i]+fila2[i]+fila3[i]+fila4[i]+fila5[i])
-
-print()
-print("SUMA POR COLUMNAS")
-for n in range(5):
-    print("Suma de la columna" + str(n+1) + ": " + str(lista_suma_columna[n]))
-
-
-print()
-print("SUMA POR FILAS")
-print("Suma fila1:", suma_fila1)
-print("Suma fila2:", suma_fila2)
-print("Suma fila3:", suma_fila3)
-print("Suma fila4:", suma_fila4)
-print("Suma fila5:", suma_fila5)
+    suma_filas=0
+    suma_columnas=0
+    for x in range(5):
+        suma_filas+=list_total[i][x]
+        suma_columnas+=list_total[x][i]
+    lista_suma_filas.append(suma_filas)
+    lista_suma_columnas.append(suma_columnas)
+        
+print(lista_suma_columnas)        
+print(lista_suma_filas)        
