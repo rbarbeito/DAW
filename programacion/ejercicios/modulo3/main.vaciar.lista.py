7adb5a7dff0bc9b2164bf6227992271b3f8376f0
Rodolfo Barbeito <rbarbeito@gmail.com>
2023-11-16 12:51:23 +0100

    Completamos ejercicio de vaciar lista

---
+++ programacion/ejercicios/modulo3/main.vaciar.lista.py
@@ -0,0 +1,11 @@
+lista = [1, 3, 4, 5, 6, 7, 82]
+# rango = range(len(lista))
+contador=len(lista)
+
+while contador >0:
+    contador+=-1
+    del lista[contador]
+    print(lista)
+    
+print()
+print("La lista esta vacia")
