c73601a6b5cba3962edf04f5cca48f8de168348f
Rodolfo Barbeito <rbarbeito@gmail.com>
2023-11-16 12:46:30 +0100

    Adicionamos main3.6.1.9.py

---
+++ programacion/ejercicios/modulo3/main3.6.1.9.py
@@ -0,0 +1,13 @@
+﻿my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+#
+# Escribe tu código aquí.
+#
+
+list_unicos=[]
+for i in range(len(my_list)):
+    if my_list[i] not in list_unicos:
+        list_unicos.append(my_list[i])
+        
+my_list=list_unicos        
+print("La lista con elementos únicos:")
+print(my_list)
